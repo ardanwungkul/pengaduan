e967d637fec77b7b9574d2dd0313ea55be3e08ff
ardanwungkul <ardanwungkul14@gmail.com>
2024-12-24 11:58:27 +0700

    a

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,13 +5,13 @@
 
     <div class="">
         <div class=" mx-auto space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white shadow-lg rounded-lg">
                 <div>
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white shadow-lg rounded-lg">
                 <div>
                     @include('profile.partials.update-password-form')
                 </div>
